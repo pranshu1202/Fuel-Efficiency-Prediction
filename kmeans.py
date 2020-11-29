512a7e67bd851e082fb1d49d34198d4b52358a49
pranshu1202 <57661561+pranshu1202@users.noreply.github.com>
2020-11-29 16:09:07 +0530

    Add files via upload

---
+++ kmeans.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as numpy
+
+class kmeans:
+    def __init__(self,input_data,output_data):
+        self.input_data = input_data
+        self.output_data = output_data
+        
