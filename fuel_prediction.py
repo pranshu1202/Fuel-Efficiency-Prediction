13fb04de6ff769c81c652571bee6698f2b49c3ca
pranshu1202 <57661561+pranshu1202@users.noreply.github.com>
2020-11-29 16:10:30 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
+from linear_regression import linear_regression 
+from kmeans import kmeans
 
 column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight','Acceleration', 'Model Year', 'Origin'] 
 data = pd.read_csv('./Data/auto-mpg.data.csv',names=column_names,index_col=False).iloc[1:].values
 
-print(data.shape)
+input_data = data[:,1:] 
+output_data = data[:,0] 
+
+obj_linear_regression = linear_regression(input_data,output_data)
+
