081025804800ab54a9e52871b42357297ed78e74
pranshu1202 <57661561+pranshu1202@users.noreply.github.com>
2020-11-29 16:09:41 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as numpy
 
 class linear_regression:
-    def __init__(self):
-        pass
+    def __init__(self,input_data,output_data):
+        self.input_data = input_data
+        self.output_data = output_data
+        
         
