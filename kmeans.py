e29bc444e9295e66578e251d1e787ba23e52d536
pranshu1202 <57661561+pranshu1202@users.noreply.github.com>
2020-12-09 00:05:04 +0530

    Delete kmeans.py

--- kmeans.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as numpy
-
-class kmeans:
-    def __init__(self,input_data,output_data):
-        self.input_data = input_data
-        self.output_data = output_data
-        
